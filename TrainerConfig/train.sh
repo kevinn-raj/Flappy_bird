8f3c43e5f8b7b9d751fff08e0a8245e9dc03d621
kevinn <kevinn@aims.ac.za>
2023-04-30 14:21:08 +0200

    Trained with curriculum over 1.5M steps(solver)

--- TrainerConfig/train.sh
+++ TrainerConfig/train.sh
@@ -1,6 +1,6 @@
 # run with 50 instances
-env="/home/bears_bears/Unity_projects/Flappy_bird/Builds/Linux_server/5_curr/Flappy_bird_RL_curriculum_5.x86_64"
-run_name=curriculum5
+env="/home/bears_bears/Unity_projects/Flappy_bird/Builds/Linux_server/6_rand/Flappy_bird_RL_rand_6.x86_64"
+run_name=rand6
 
 #resume previous training
 #mlagents-learn ARLPCG_curriculum.yaml --num-envs 50 --run-id run_name --no-graphics --env env --resume
@@ -8,7 +8,8 @@ run_name=curriculum5
 
 #mlagents-learn ARLPCG_curriculum.yaml --run-id test_newGen --force # new generator
 
-mlagents-learn ARLPCG_curriculum_2.yaml --num-envs 40 --run-id $run_name --env $env --resume --no-graphics --time-scale 20
+#mlagents-learn ARLPCG_curriculum_2.yaml --num-envs 40 --run-id $run_name --env $env --resume --no-graphics --time-scale 20
+mlagents-learn ARLPCG_rand_6.yaml --num-envs 40 --run-id $run_name --env $env --no-graphics --time-scale 20 #rand_6
 
- 
+
 
