f750a534cb6ff822cea83cebd389d4aba97c2096
Kevinn Rajaona <kevinn@aims.ac.za>
2023-05-04 21:20:49 +0300

    25c optimizing Solver learning rate

--- TrainerConfig/train.bat
+++ TrainerConfig/train.bat
@@ -1,7 +1,7 @@
 @echo off
 REM run with 50 instances
 set env="Z:\Git\Flappy_bird\Builds\Windows\Curr25b\Flappy_bird.exe"
-set run_name=Curr25b
+set run_name=Curr25c
 
 REM resume previous training
 REM mlagents-learn ARLPCG_curriculum.yaml --num-envs 50 --run-id run_name --no-graphics --env env --resume
@@ -14,6 +14,6 @@ REM mlagents-learn ARLPCG_curriculum_2.yaml --num-envs 40 --run-id $run_name --e
 :: Execute this
 ::mlagents-learn ARLPCG_curriculum_6_GPU.yaml --num-envs=20 --run-id=%run_name% --env=%env% --no-graphics --torch-device=cuda --time-scale=5
 
-mlagents-learn ARLPCG_curriculum_6_GPU.yaml --num-envs=20 --run-id=%run_name% --env=%env%   --no-graphics --torch-device=cuda --time-scale=7 --force
+mlagents-learn ARLPCG_curriculum_6_GPU.yaml --num-envs=20 --run-id=%run_name% --env=%env%   --no-graphics --torch-device=cuda --time-scale=7  
 
 
