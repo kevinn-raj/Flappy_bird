6e0035f226411097910867a7696a001075b121a4
Kevinn Rajaona <kevinn@aims.ac.za>
2023-05-04 22:57:12 +0300

    Curr27
    Changed jump force, time window

--- TrainerConfig/train.bat
+++ TrainerConfig/train.bat
@@ -1,7 +1,7 @@
 @echo off
 REM run with 50 instances
-set env="Z:\Git\Flappy_bird\Builds\Windows\Curr26\Flappy_bird.exe"
-set run_name=Curr26
+set env="Z:\Git\Flappy_bird\Builds\Windows\Curr27\Flappy_bird.exe"
+set run_name=Curr27
 
 REM resume previous training
 REM mlagents-learn ARLPCG_curriculum.yaml --num-envs 50 --run-id run_name --no-graphics --env env --resume
