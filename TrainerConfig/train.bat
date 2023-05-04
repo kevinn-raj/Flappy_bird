18ca97631f3cc9aa8d44369bef0fcc67cc81008d
Kevinn Rajaona <kevinn@aims.ac.za>
2023-05-04 23:38:31 +0300

    Trained Curr27

--- TrainerConfig/train.bat
+++ TrainerConfig/train.bat
@@ -1,7 +1,7 @@
 @echo off
 REM run with 50 instances
 set env="Z:\Git\Flappy_bird\Builds\Windows\Curr27\Flappy_bird.exe"
-set run_name=Curr27
+set run_name=Curr27a
 
 REM resume previous training
 REM mlagents-learn ARLPCG_curriculum.yaml --num-envs 50 --run-id run_name --no-graphics --env env --resume
