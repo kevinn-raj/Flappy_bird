2f68258cae4b6f019f01fe97b6fd9d1940aff809
Kevinn Rajaona <kevinn@aims.ac.za>
2023-05-13 19:36:13 +0300

    Trained a version of PCG with less varying params

--- TrainerConfig/train_PCG.bat
+++ TrainerConfig/train_PCG.bat
@@ -1,7 +1,7 @@
 @echo off
 REM run with 50 instances
-set env="Z:\Git\Flappy_bird\Builds\Windows\PCG_2\Flappy_bird.exe"
-set run_name=PCG_2 
+set env="Z:\Git\Flappy_bird\Builds\Windows\PCG_Const_1\Flappy_bird.exe"
+set run_name=PCG_Const_1
 REM resume previous training
 REM mlagents-learn ARLPCG_curriculum.yaml --num-envs 50 --run-id run_name --no-graphics --env env --resume
 REM mlagents-learn ARLPCG_curriculum.yaml --num-envs 50 --run-id $run_name --env $env --resume --no-graphics 
@@ -11,7 +11,7 @@ REM mlagents-learn ARLPCG_curriculum.yaml --run-id test_newGen --force # new gen
 REM mlagents-learn ARLPCG_curriculum_2.yaml --num-envs 40 --run-id $run_name --env $env --resume --no-graphics --time-scale 20
 
 :: Execute this
-mlagents-learn PCG.yaml  --num-envs=10 --run-id=%run_name%  --no-graphics --torch-device=cuda --time-scale=1  --env=%env%
+mlagents-learn PCG.yaml  --num-envs=10 --run-id=%run_name%  --no-graphics --torch-device=cuda --time-scale=1  --env=%env% --force
 
 ::mlagents-learn easy.yaml --num-envs=7 --run-id=%run_easy% --env=%env%   --no-graphics --torch-device=cuda --time-scale=3  --base-port=5000 
 ::
