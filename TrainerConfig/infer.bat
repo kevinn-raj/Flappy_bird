46f01d24214befbfefe35eeaf81d55b9b87b000d
Kevinn Rajaona <kevinn@aims.ac.za>
2023-05-14 20:02:56 +0300

    Infered Curr42d solver on NoCurr

--- TrainerConfig/infer.bat
+++ TrainerConfig/infer.bat
@@ -1,8 +1,8 @@
 @echo off
 REM run with 50 instances
-set env="Z:\Git\Flappy_bird\Builds\Windows\Inference\Flappy_bird.exe"
-set run_name=InferPCG_Const_1_aux_0
-set initRUN_ID=PCG_Const_1
+set env="Z:\Git\Flappy_bird\Builds\Windows\InferenceNoCurr\Flappy_bird.exe"
+set run_name=InferenceNoCurr_Curr42d_1
+set initRUN_ID=Curr42d
 REM resume previous training
 REM mlagents-learn ARLPCG_curriculum.yaml --num-envs 50 --run-id run_name --no-graphics --env env --resume
 REM mlagents-learn ARLPCG_curriculum.yaml --num-envs 50 --run-id $run_name --env $env --resume --no-graphics 
